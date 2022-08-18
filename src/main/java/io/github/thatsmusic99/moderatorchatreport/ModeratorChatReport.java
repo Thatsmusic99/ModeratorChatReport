e5986ef9a60c38176c22aa33c3f9f6e2b3a6b09a
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2022-08-18 13:27:33 +0100

    Initial commit

---
+++ src/main/java/io/github/thatsmusic99/moderatorchatreport/ModeratorChatReport.java
@@ -0,0 +1,17 @@
+package io.github.thatsmusic99.moderatorchatreport;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ModeratorChatReport extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
